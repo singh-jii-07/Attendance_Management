82dddf599cb981785b8ea5d2d369c3c81a66935b
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-04-01 21:45:33 +0530

    mogodb connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,26 @@
-console.log("hello");
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());          
+app.use(express.json()); 
+
+
+mongoose.connect(process.env.MongoDb_url)
+.then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+
+.catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+});
