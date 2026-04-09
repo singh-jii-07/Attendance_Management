1ef5b5a42d7010468755dd9e8d85fa1da5dfd2fd
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-04-09 21:55:49 +0530

    login controller done

--- Backend/App/Controller/Admin.js
+++ Backend/App/Controller/Admin.js
@@ -1,10 +1,11 @@
 import Admin from "../Models/Admin.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const register = async (req, res) => {
   try {
-    const { name, password, email,role } = req.body;
+    const { name, password, email, role } = req.body;
 
-    if (!name || !password || !email) {
+    if (!name || !password || !email || role) {
       return res.status(400).json({
         message: "All fields are required",
       });
@@ -20,7 +21,7 @@ const register = async (req, res) => {
       name,
       email,
       password: hashedPassword,
-      role
+      role,
     });
 
     return res.status(201).json({
@@ -37,4 +38,47 @@ const register = async (req, res) => {
   }
 };
 
-export {register}
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+  const checkAdmin = await Admin.findOne({ email });
+  if (!checkAdmin) {
+    return res.status(400).json({
+      message: "User not found",
+    });
+  }
+
+  const ismatch = await bcrypt.compare(password, checkuser.password);
+  try {
+    if (!ismatch) {
+      return res.status(400).json({
+        message: "password is wrong",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: checkAdmin._id, role: checkAdmin.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    return res.status(200).json({
+      message: "User login successful",
+      token,
+    });
+  } catch (err) {
+    console.error("Login Error", err);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
+
+export { register,login };
