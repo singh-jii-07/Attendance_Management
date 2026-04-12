3ae7f69d9eff44132b0eaebf8c5640da5bb518d1
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-04-12 20:50:57 +0530

    logout routes done

--- Backend/App/Routes/Admin.js
+++ Backend/App/Routes/Admin.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, profile, register } from '../Controller/Admin.js'
+import { login, logout, profile, register } from '../Controller/Admin.js'
 import Auth from '../Middleware/Auth.js';
 
 const adminRoute=express.Router();
@@ -7,5 +7,6 @@ const adminRoute=express.Router();
 adminRoute.post("/register",register)
 adminRoute.post("/login",login)
 adminRoute.get("/profile",Auth,profile)
+adminRoute.post("/logout",logout)
 
 export default adminRoute;
