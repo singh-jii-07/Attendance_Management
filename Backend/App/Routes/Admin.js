b1e60513ef615a3c5bf93def12eba2b89540abda
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-04-12 20:45:56 +0530

    profile routes done

--- Backend/App/Routes/Admin.js
+++ Backend/App/Routes/Admin.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { login, register } from '../Controller/Admin.js'
+import { login, profile, register } from '../Controller/Admin.js'
+import Auth from '../Middleware/Auth.js';
 
 const adminRoute=express.Router();
 
 adminRoute.post("/register",register)
 adminRoute.post("/login",login)
+adminRoute.get("/profile",Auth,profile)
 
 export default adminRoute;
